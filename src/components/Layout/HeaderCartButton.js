ef54786f551b8b1605dce763dd7df4a241d99636
Simon Pletersek <pletersek.si@gmail.com>
2023-05-16 19:56:01 +0200

    Added the function to remove items from the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,46 @@
+import { useContext, useEffect, useState } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHightligted] = useState(false);
+    const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const btnClasses = `${classes.button} ${
+        btnIsHighlighted ? classes.bump : ''
+    }`;
+
+    // Bump animation
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHightligted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHightligted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
         <button
-            onClick={props.onClick}
-            className={classes['button']}>
+            className={btnClasses}
+            onClick={props.onClick}>
             <span className={classes['icon']}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes['badge']}>1</span>
+            <span className={classes['badge']}>{numberOfCartItems}</span>
         </button>
     );
 };
