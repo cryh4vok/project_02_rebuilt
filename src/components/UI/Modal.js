8d57db8044a57fa939ceb5e314bd728e20013f8c
Simon Pletersek <pletersek.si@gmail.com>
2023-05-17 12:49:51 +0200

    Cart state complete -> can now close and open the cart

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,38 @@
+import classes from './Modal.module.css';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+//! These two components are closely tied to Modal
+const Backdrop = (props) => {
+    return (
+        <div
+            onClick={props.onClose}
+            className={classes['backdrop']}></div>
+    );
+};
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes['modal']}>
+            <div className={classes['content']}>{props.children}</div>
+        </div>
+    );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClose={props.onClose} />,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+        </Fragment>
+    );
+};
+
+export default Modal;
